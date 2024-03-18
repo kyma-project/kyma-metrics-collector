13e11e5059d384bd9b363376166bcbbd3094215c
Muhammad Faizan <mfaizanse@users.noreply.github.com>
2024-03-18 11:30:27 +0100

    chore: implement automated release workflow - part 1 (#16)
    * chore: implement automated release workflow - part 1
    
    * updated github bot email

---
+++ hack/scripts/await_pr_merge.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+# Expected variables:
+#             KYMA_METRICS_COLLECTOR_REPO - Kyma repository
+#             PR_NUMBER - Number of the PR with the changes to be merged
+
+
+until  $(gh pr view ${PR_NUMBER} --json closed | jq -r '.closed'); do
+  echo "Waiting for https://github.com/${KYMA_METRICS_COLLECTOR_REPO}/pull/${PR_NUMBER} to be merged"
+  sleep 5
+done
