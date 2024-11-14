035da9a9a1fbc67930a8c44a98e7da0cc68c63a7
Korbinian Stoemmer <korbinian.stoemmer@sap.com>
2024-11-14 15:06:49 +0100

    lint fix

--- pkg/cache/metrics.go
+++ pkg/cache/metrics.go
@@ -5,15 +5,13 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-var (
-	cacheSizeMetric = promauto.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Namespace: "kmc",
-			Subsystem: "kubeconfig_cache",
-			Name:      "size",
-			Help:      "Number of items in the kubeconfig cache.",
-		}, nil)
-)
+var cacheSizeMetric = promauto.NewGaugeVec(
+	prometheus.GaugeOpts{
+		Namespace: "kmc",
+		Subsystem: "kubeconfig_cache",
+		Name:      "size",
+		Help:      "Number of items in the kubeconfig cache.",
+	}, nil)
 
 func recordMetrics() {
 	cacheSizeMetric.With(prometheus.Labels{}).Set(float64(kubeConfigCache.Len()))
