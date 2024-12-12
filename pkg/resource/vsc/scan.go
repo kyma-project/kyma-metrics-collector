37eebc424f54fb9b8600b4d7add2676254b0afdb
Hisar Balik <hisar.balik@sap.com>
2024-12-12 15:33:16 +0100

    add billing metrics for VolumeSnapShotContents

---
+++ pkg/resource/vsc/scan.go
@@ -0,0 +1,55 @@
+package vsc
+
+import (
+	"errors"
+	"math"
+	"time"
+
+	v1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
+
+	"github.com/kyma-project/kyma-metrics-collector/pkg/resource"
+)
+
+const (
+	// storageRoundingFactor rounds of storage to 32. E.g. 17 -> 32, 33 -> 64.
+	storageRoundingFactor = 32
+
+	GiB = 1 << (10 * 3) //nolint:mnd // 1 GiB = 1024^3 bytes
+)
+
+var _ resource.ScanConverter = &Scan{}
+
+type Scan struct {
+	vscs v1.VolumeSnapshotContentList
+}
+
+func (s *Scan) UM(duration time.Duration) (resource.UMMeasurement, error) {
+	return resource.UMMeasurement{}, nil
+}
+
+func (s *Scan) EDP() (resource.EDPMeasurement, error) {
+	edp := resource.EDPMeasurement{}
+
+	var errs []error
+
+	for _, vsc := range s.vscs.Items {
+		if vsc.Status.ReadyToUse != nil && *vsc.Status.ReadyToUse {
+			currVSC := getSizeInGB(*vsc.Status.RestoreSize)
+			edp.ProvisionedVolumes.SizeGbTotal += currVSC
+			edp.ProvisionedVolumes.SizeGbRounded += getVolumeRoundedToFactor(currVSC)
+			edp.ProvisionedVolumes.Count += 1
+		}
+	}
+
+	return edp, errors.Join(errs...)
+}
+
+func getSizeInGB(value int64) int64 {
+	gVal := int64(float64(value) / GiB)
+
+	return gVal
+}
+
+func getVolumeRoundedToFactor(size int64) int64 {
+	return int64(math.Ceil(float64(size)/storageRoundingFactor) * storageRoundingFactor)
+}
---
+++ pkg/resource/vsc/scan_test.go
@@ -0,0 +1,88 @@
+package vsc
+
+import (
+	"testing"
+
+	v1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
+	"github.com/stretchr/testify/require"
+	"k8s.io/utils/ptr"
+
+	"github.com/kyma-project/kyma-metrics-collector/pkg/resource"
+)
+
+func TestScan_EDP(t *testing.T) {
+	tests := []struct {
+		name     string
+		vscs     v1.VolumeSnapshotContentList
+		expected resource.EDPMeasurement
+	}{
+		{
+			name: "no vscs",
+			vscs: v1.VolumeSnapshotContentList{},
+			expected: resource.EDPMeasurement{
+				ProvisionedVolumes: resource.ProvisionedVolumes{
+					SizeGbTotal:   0,
+					SizeGbRounded: 0,
+					Count:         0,
+				},
+			},
+		},
+		{
+			name: "single vsc",
+			vscs: v1.VolumeSnapshotContentList{
+				Items: []v1.VolumeSnapshotContent{
+					{
+						Status: &v1.VolumeSnapshotContentStatus{
+							ReadyToUse:  ptr.To(true),
+							RestoreSize: ptr.To(int64(10737418240)), // 10GB
+						},
+					},
+				},
+			},
+			expected: resource.EDPMeasurement{
+				ProvisionedVolumes: resource.ProvisionedVolumes{
+					SizeGbTotal:   10,
+					SizeGbRounded: 32, // Rounded to 32 GiB
+					Count:         1,
+				},
+			},
+		},
+		{
+			name: "mixed vscs",
+			vscs: v1.VolumeSnapshotContentList{
+				Items: []v1.VolumeSnapshotContent{
+					{
+						Status: &v1.VolumeSnapshotContentStatus{
+							ReadyToUse:  ptr.To(true),
+							RestoreSize: ptr.To(int64(10737418240)), // 10GB
+						},
+					},
+					{
+						Status: &v1.VolumeSnapshotContentStatus{
+							ReadyToUse:  ptr.To(true),
+							RestoreSize: ptr.To(int64(2 * 10737418240)), // 20GB
+						},
+					},
+				},
+			},
+			expected: resource.EDPMeasurement{
+				ProvisionedVolumes: resource.ProvisionedVolumes{
+					SizeGbTotal:   30, // 10 + 20
+					SizeGbRounded: 64, // 32 (rounded 10) + 64 (rounded 60)
+					Count:         2,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			scan := &Scan{
+				vscs: test.vscs,
+			}
+			actual, err := scan.EDP()
+			require.NoError(t, err)
+			require.Equal(t, test.expected, actual)
+		})
+	}
+}
