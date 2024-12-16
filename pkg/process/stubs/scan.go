efe94fe9edaa7718828b75f24e84544a3d80f3a2
Mostafa Shorim <mostafa.shorim@sap.com>
2024-12-16 04:58:25 +0100

    adjust existing unit tests in process_test.go

---
+++ pkg/process/stubs/scan.go
@@ -0,0 +1,24 @@
+package stubs
+
+import (
+	"github.com/kyma-project/kyma-metrics-collector/pkg/resource"
+	"time"
+)
+
+type Scan struct {
+	resources []string
+}
+
+func NewScan(resources []string) Scan {
+	return Scan{
+		resources: resources,
+	}
+}
+
+func (s Scan) UM(duration time.Duration) (resource.UMMeasurement, error) {
+	return resource.UMMeasurement{}, nil
+}
+
+func (s Scan) EDP() (resource.EDPMeasurement, error) {
+	return resource.EDPMeasurement{}, nil
+}
