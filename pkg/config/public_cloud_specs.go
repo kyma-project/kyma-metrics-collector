637974572e41a5dd9a1527477966aa38f0ed0866
Andreas Thaler <andreas.thaler01@sap.com>
2025-02-06 18:04:09 +0100

    chore: Remove dead config code

--- pkg/config/public_cloud_specs.go
+++ pkg/config/public_cloud_specs.go
@@ -32,8 +32,6 @@ type Providers struct {
 type Feature struct {
 	CpuCores int     `json:"cpu_cores"`
 	Memory   float64 `json:"memory"`
-	Storage  int     `json:"storage,omitempty"`
-	MaxNICs  int     `json:"max_nics,omitempty"`
 }
 
 type RedisInfo struct {
