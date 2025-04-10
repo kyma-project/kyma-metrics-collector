d43b8f9670aa2b3ee24b416d1cc0f359796568a8
Rakesh Garimella <rakesh.garimella@sap.com>
2025-04-10 14:39:11 +0200

    fixes

--- pkg/process/filter_clusters.go
+++ pkg/process/filter_clusters.go
@@ -1,7 +1,9 @@
 package process
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 	"gopkg.in/yaml.v3"
@@ -24,6 +26,7 @@ func parseRuntimesToBeFiltered(data []byte) (map[string]struct{}, error) {
 	var filter accounts
 
 	skippedAccounts := make(map[string]struct{})
+	invalidGlobalAccount := []string{}
 
 	err := yaml.Unmarshal(data, &filter)
 	if err != nil {
@@ -32,11 +35,15 @@ func parseRuntimesToBeFiltered(data []byte) (map[string]struct{}, error) {
 
 	for _, account := range filter.SkippedGlobalAccounts {
 		if err = uuid.Validate(account); err != nil {
+			invalidGlobalAccount = append(invalidGlobalAccount, account)
 			continue
 		}
 
 		skippedAccounts[account] = struct{}{}
 	}
 
+	if len(invalidGlobalAccount) > 0 {
+		return nil, fmt.Errorf("invalid global account IDs: %s", strings.Join(invalidGlobalAccount, ", "))
+	}
 	return skippedAccounts, nil
 }
--- pkg/process/filter_clusters_test.go
+++ pkg/process/filter_clusters_test.go
@@ -1,6 +1,7 @@
 package process
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,22 +9,25 @@ import (
 
 func TestParseClusterToBeFiltered(t *testing.T) {
 	tt := []struct {
-		name     string
-		cluster  string
-		expected map[string]struct{}
+		name        string
+		cluster     string
+		expected    map[string]struct{}
+		expectedErr error
 	}{
 		{
 			name: "file with single cluster",
 			cluster: `globalAccounts: 
                        - "8946D5DE-E59A-4F4D-B65D-4595758D1FB1"`,
-			expected: map[string]struct{}{"8946D5DE-E59A-4F4D-B65D-4595758D1FB1": {}},
+			expected:    map[string]struct{}{"8946D5DE-E59A-4F4D-B65D-4595758D1FB1": {}},
+			expectedErr: nil,
 		},
 		{
 			name: "with duplicate cluster names",
 			cluster: `globalAccounts:
                        - "8946D5DE-E59A-4F4D-B65D-4595758D1FB1"
                        - "8946D5DE-E59A-4F4D-B65D-4595758D1FB1"`,
-			expected: map[string]struct{}{"8946D5DE-E59A-4F4D-B65D-4595758D1FB1": {}},
+			expected:    map[string]struct{}{"8946D5DE-E59A-4F4D-B65D-4595758D1FB1": {}},
+			expectedErr: nil,
 		},
 		{
 			name: "with multiple clusters",
@@ -34,23 +38,35 @@ func TestParseClusterToBeFiltered(t *testing.T) {
 				"8946D5DE-E59A-4F4D-B65D-4595758D1FB1": {},
 				"E653F9B0-97F1-4BF4-AAF2-268C5217CF49": {},
 			},
+			expectedErr: nil,
 		},
 		{
-			name:     "empty cluster list",
-			cluster:  ``,
-			expected: map[string]struct{}{},
+			name: "with invalid global account ID",
+			cluster: `globalAccounts:
+                       - "a8946D5DE-E59A-4F4D-B65D-4595758D1FB1"
+                       - "bE653F9B0-97F1-4BF4-AAF2-268C5217CF49"`,
+			expected:    nil,
+			expectedErr: fmt.Errorf("invalid global account IDs: a8946D5DE-E59A-4F4D-B65D-4595758D1FB1, bE653F9B0-97F1-4BF4-AAF2-268C5217CF49"),
 		},
 		{
-			name:     "with invalid YAML",
-			cluster:  `"invalid: yaml"`,
-			expected: nil,
+			name:        "empty cluster list",
+			cluster:     ``,
+			expected:    map[string]struct{}{},
+			expectedErr: nil,
+		},
+		{
+			name:        "with invalid YAML",
+			cluster:     `"invalid: yaml"`,
+			expected:    nil,
+			expectedErr: fmt.Errorf("yaml: unmarshal errors:\n  line 1: cannot unmarshal !!str `invalid...` into process.accounts"),
 		},
 	}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
 			actual, err := parseRuntimesToBeFiltered([]byte(tc.cluster))
-			if tc.expected == nil {
+			if tc.expectedErr != nil {
 				require.Error(t, err)
+				require.Equal(t, tc.expectedErr.Error(), err.Error())
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, tc.expected, actual)
